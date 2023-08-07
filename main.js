66c1273236660cef74951fffabe79753b1729334
CloudyChan <107761059+CloudyChan@users.noreply.github.com>
2023-08-07 11:42:22 +0100

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function discount(value, discount) {
-    return (value/discount)*100;
-}
-
-var menu = [
-    ["Bacon Cheeseburger", "Burgerz", 3.50, "20%"],
-    ["Chicken Burger", "Burger", 3.20, "20%"],
-    ["Beef Burger", "Burger", 3.00, "20%"]
-    ];
