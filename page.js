c1a9ba1f7bc502ac440ee9d37b643947093e3e75
CloudyChan <107761059+CloudyChan@users.noreply.github.com>
2023-08-07 13:57:40 +0100

    Update page.js

--- page.js
+++ page.js
@@ -45,3 +45,34 @@ sr.reveal(`.home-text, .home-img,
 function openContactForm() {
     window.open(contactLink);
 }
+
+// Cookies
+const cookieBox = document.querySelector(".cookies"),
+  buttons = document.querySelectorAll(".button");
+
+const executeCodes = () => {
+  // if cookie contains Taste it will be returned and below of this code will not run
+  if (document.cookie.includes("Taste")) return;
+  cookieBox.classList.add("show");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      cookieBox.classList.remove("show");
+
+      // if button has acceptBtn id
+      if (button.id == "acceptBtn") {
+        // set cookies for 1 month. 60 = 1 min, 60 = 1 hours, 24 = 1 day, 30 = 30 days
+        document.cookie = "cookieBy= Taste; max-age=" + 60 * 60 * 24 * 30;
+      }
+    });
+  });
+};
+
+// executeCodes function will be called on webpage load
+window.addEventListener("load", executeCodes);
+
+window.addEventListener('keydown', function(e) {
+    if(e.keyCode == 32 && e.target == document.body) {
+      e.preventDefault();
+    }
+});
